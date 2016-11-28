1da4871159ae3fced351206e271ec561fb102f7e
Michael J. Ryan <tracker1@gmail.com>
2016-11-28 12:57:13 -0500

    correct static asset path...

--- src/server/middleware/static.js
+++ src/server/middleware/static.js
@@ -2,5 +2,5 @@ import path from 'path';
 import express from 'express';
 
 export default async function registerApp(app) {
-  app.use('/public', express.static(path.resolve(__dirname, '../../../static')));
+  app.use('/public', express.static(path.resolve(__dirname, '../../public')));
 }
